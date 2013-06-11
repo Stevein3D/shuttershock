f2afe2efca4515dbdf62a0aab757fe5d6b315058
Steve Sikoryak <stevein3d@gmail.com>
2013-06-11 13:51:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shuttershock::Application.config.secret_key_base = '3e86b2a92bdaf06213db25fecf8fad328bf64373412a3cdd0684934b3fba9adbba60e83cde468fbdef4adf4b222bdaf28b54c77ddffb2fdb62235970f7e19812'
